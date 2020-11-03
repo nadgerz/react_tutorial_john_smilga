2c51f3a9916aa929f88f486132308562c669b671
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2020-11-03 13:57:12 +0100

    under the hood

--- smilga/src/index.tsx
+++ smilga/src/index.tsx
@@ -5,4 +5,10 @@ function Greeting() {
   return <h4>This is Steve and this is my first component</h4>;
 }
 
+/*
+const Greeting = () => {
+	return React.createElement('h1', {}, 'Booyah!')
+}
+ */
+
 ReactDom.render(<Greeting />, document.getElementById("root"));
