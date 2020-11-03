4ceae614cba52e61e751872cd888c87810c41838
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2020-11-03 13:59:39 +0100

    src/index.tsx   Line 10:11:  The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value. If you cannot provide a valid href, but still need the element to resemble a link, use a button and change it with appropriate styles. Learn more: https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md  jsx-a11y/anchor-is-valid
    Search for the keywords to learn more about each warning.
    To ignore, add // eslint-disable-next-line to the line before.

--- smilga/src/index.tsx
+++ smilga/src/index.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDom from "react-dom";
 
 function Greeting() {
-  return <h4>This is Steve and this is my first component</h4>;
+  return (
+    <div>
+      <h3>hello people</h3>
+      <ul>
+        <li>
+          <a href="#">hello again</a>
+        </li>
+      </ul>
+    </div>
+  );
 }
-
-/*
-const Greeting = () => {
-	return React.createElement('h1', {}, 'Booyah!')
-}
- */
 
 ReactDom.render(<Greeting />, document.getElementById("root"));
