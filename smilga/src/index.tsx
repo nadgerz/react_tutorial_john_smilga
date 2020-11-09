f375be9946a93d115e1f60359659478c0d2a1b54
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2020-11-09 13:22:40 +0100

    more books

--- smilga/src/index.tsx
+++ smilga/src/index.tsx
@@ -2,11 +2,20 @@ import React from "react";
 import ReactDom from "react-dom";
 
 function BookList() {
-  return <section>this is a booklist</section>;
+  return (
+    <>
+      <section>this is a booklist</section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </>
+  );
 }
 
 const Book = () => {
-  return <article></article>;
+  return <article>this is a book</article>;
 };
 
 ReactDom.render(<BookList />, document.getElementById("root"));
